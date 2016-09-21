fc6db099764fe4fa2975f2e703cd7e8143b0f4b4
Josh Huffman and Ryan Loos <josh@>
2016-09-21 11:53:00 -0700

    add front end

--- app/app.php
+++ app/app.php
@@ -1,12 +1,13 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/template.php";
+    require_once __DIR__."/../src/Restaurant.php";
+    require_once __DIR__."/../src/Cuisine.php";
 
-    session_start();
-    if (empty($_SESSION['collection'])) {
-        $_SESSION['collection'] = array();
-    }
+    $server = 'mysql:host=localhost;dbname=best_restaurants';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
     $app = new Silex\Application();
 
@@ -18,7 +19,36 @@
 
   //loads actual twig file
     $app->get("/", function() use ($app) {
-      return $app['twig']->render("home.html.twig");
+      return $app['twig']->render("home.html.twig", array('restaurants' => Restaurant::getAll()));
+    });
+
+    $app->post("/restaurants", function() use($app) {
+        $name = $_POST['restaurant_name'];
+        $description = $_POST['restaurant_description'];
+        $inputCuisine = strtolower($_POST['restaurant_cuisine']);
+        $cuisines = Cuisine::getAll();
+        $cuisine_id = null;
+        foreach ($cuisines as $cuisine) {
+            if ($cuisine->getCuisineType() == $inputCuisine) {
+                $cuisine_id = $cuisine->getId();
+            }
+        }
+        if ($cuisine_id == null) {
+            $newCuisine = new Cuisine($inputCuisine);
+            $newCuisine->save();
+            $cuisine_id = $newCuisine->getId();
+        }
+
+        $newRestaurant = new Restaurant($name, $description, $cuisine_id);
+        $newRestaurant->save();
+
+        return $app['twig']->render("restaurants.html.twig", array('restaurants' => Restaurant::getAll(), 'cuisines' => Cuisine::getAll()));
+    });
+
+    $app->get("/restaurant/{id}", function($id) use ($app) {
+        $restaurant = Restaurant::find($id);
+        $cuisine = Cuisine::find($restaurant->getCuisineId());
+        return $app['twig']->render("restaurant.html.twig", array('restaurant' => $restaurant, 'cuisine' =>$cuisine));
     });
 
   //loads basic php
